a20b6c8dfdf80953d9808a510af044c05c88a411
koomekevo <koome.kevo@gurucrafts.net>
2022-06-07 12:16:17 +0300

    Add search function in search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,9 @@
 class SearchController < ApplicationController
+  def search
+    @films = if params[:term].nil?
+               []
+             else
+               Film.search params[:term]
+             end
+  end
 end
